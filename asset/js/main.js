21e844cb0d5b5f5f8d56df129748f784b04a6333
Yazawa Kotaro <fragancia.buena@gmail.com>
2020-06-28 10:48:40 +0900

    IEでの初期表示の設定 コメントをわかりやすく表記

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,15 @@
 'use strict';
 
 $(function(){
-  // トップページの最初の表示
+  // ページ表示時のエフェクト用
   setTimeout(() => {
     $('.starting').fadeOut(1300);
   }, 200);
   setTimeout(() => {
     $('.starting-page').fadeOut(650);
   }, 200);
-  // コンタクト項目ホバー時変化
+
+  // ヘッダのコンタクト項目ホバー時アイコン変化
   $('.contact-list a').hover(function(){
     $(this).find('.mail').removeClass('fa-envelope').addClass('fa-envelope-open');
     $(this).find('.teams').css('margin-right','5px').removeClass('fa-user-friends').addClass('fa-comment-dots');
@@ -17,7 +18,7 @@ $(function(){
     $(this).find('.teams').css('margin-right','3px').removeClass('fa-comment-dots').addClass('fa-user-friends');
   });
 
-  // スクロール時にナビ固定
+  // スクロール時にナビメニュー固定
   const headerPos = $('header').outerHeight();
   $(window).scroll(function(){
     if($('body, html').scrollTop() > headerPos){
@@ -28,15 +29,17 @@ $(function(){
         'left' : '50%',
         'transform' : 'translateX(-50%)',
         'margin' : '8px 0 10px 0',
-        'background-color' : 'rgba(18,39,74, .6)',
+        'background-color' : 'rgba(18,39,74, .7)',
       });
-      $('.global-nav a').css('color', '#fff');
+      $('.global-nav a').css(
+        'color', '#fff'
+      );
       $('.contact-list').css({
         'position' : 'fixed',
         'z-index' : '10',
         'top' : '0',
         'right' : '0',
-        'background-color' : 'rgba(18,39,74, .6)',
+        'background-color' : 'rgba(18,39,74, .7)',
         'height' : headerPos,
       });
     }else{
@@ -50,7 +53,9 @@ $(function(){
         'margin' : '',
         'background-color' : '',
       });
-      $('.global-nav a').css('color', '');
+      $('.global-nav a').css(
+        'color', ''
+      );
       $('.contact-list').css({
         'display' : '',
         'position' : '',
@@ -63,24 +68,24 @@ $(function(){
     }
   });
 
-    // スライドショー
-    const count = $('.slides li').length;
-    let current = 1;
-    let next = 2;
+    // メインビジュアルのスライドショー
+    const slideCount = $('.slides li').length;
+    let currentSlide = 1;
+    let nextSlide = 2;
     const interval = 3800;
     const duration = 700;
     let timer;
     $('.slides li:not(:first-child)').hide();
     function slideTimer(){
-      $('.slides li:nth-child('+current+')').fadeOut(duration);
-      $('.slides li:nth-child('+next+')').fadeIn(duration);
-      current = next;
-      next = ++next;
-      if(next > count){
-        next = 1;
+      $('.slides li:nth-child('+currentSlide+')').fadeOut(duration);
+      $('.slides li:nth-child('+nextSlide+')').fadeIn(duration);
+      currentSlide = nextSlide;
+      nextSlide = ++nextSlide;
+      if(nextSlide > slideCount){
+        nextSlide = 1;
       }
       $('.visual-select ul li').removeClass('selected');
-      $('.visual-select ul li:nth-child('+current+')').addClass('selected');
+      $('.visual-select ul li:nth-child('+currentSlide+')').addClass('selected');
     }
     timer = setInterval(slideTimer,interval);
   
@@ -92,14 +97,13 @@ $(function(){
       timer = setInterval(slideTimer,interval);
     });
     $('.visual-select ul li').click(function(){
-      next = $(this).html();
+      nextSlide = $(this).html();
       clearInterval(timer);
       timer = setInterval(slideTimer,interval);
       slideTimer();
       return false;
     });
-
-
+    
     // コンテンツの下部分ぼかしの制御
     $('.section-desc').scroll(function(){
       if($(this).scrollTop() > 0){
